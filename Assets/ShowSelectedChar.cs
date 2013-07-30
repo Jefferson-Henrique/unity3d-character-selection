bf4223a7f3a3b0e12ffca82f3d8aa7883089dcec
Jefferson Henrique <jeffersonpi@hotmail.com>
2013-07-30 11:07:13 -0300

    - Added a better comment

--- Assets/ShowSelectedChar.cs
+++ Assets/ShowSelectedChar.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class ShowSelectedChar : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
+		// We just have to Instantiate a new GameObject based on the selected character
 		GameObject.Instantiate(SelectCharStyled.charsPrefabsAux[SelectCharStyled.currentChar].gameObject, Vector3.zero, Quaternion.identity);
 	}
 	
